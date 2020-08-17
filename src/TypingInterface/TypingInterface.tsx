8b957e93cb49cdc58b3ea53fa72e19e8660c2ebf
Istvan Derda <istvan.derda@stud.htwk-leipzig.de>
2020-08-17 22:05:30 +0200

    clipping long lines of text

--- src/TypingInterface/TypingInterface.tsx
+++ src/TypingInterface/TypingInterface.tsx
@@ -3,33 +3,34 @@ import "./TypingInterface.scss";
 import React, {useEffect, useState} from "react";
 
 const TypingInterface = () => {
-    const [targetText, setTargetText] = useState("elcome to Typo, a typing-trainer where you decide, what you want to type!");
+    const [nextToType, setNextToType] = useState("elcome to Typo, a typing-trainer where you decide, what you want to type! \"Typo\" is an acronym for \"Your Online Typing Practices\". It is developed with React and Typescript and optimized for Mozilla Firefox.");
     const [nextChar, setNextChar] = useState("W")
     const [typedText, setTypedText] = useState("");
     const [inputText, setInputText] = useState("");
 
     useEffect(() => {
-        if (inputText === nextChar) {
+        if (inputText === nextChar && nextChar !== "") {
+            console.log(nextChar)
             setTypedText(current => current + inputText);
             setInputText("");
-            setNextChar(targetText[0]);
-            setTargetText(current => current.substr(1));
+            setNextChar(nextToType[0]);
+            setNextToType(current => current.substr(1));
         }
-    }, [inputText, nextChar, targetText])
+    }, [inputText, nextToType, inputText])
 
     return (
         <div className={"ty-typing-interface"}>
             <div className={"ty-typed-text"}>
-                <pre>{typedText}</pre>
+                <pre>{typedText.substring(typedText.length - 60 > 0 ? typedText.length - 60 : 0, typedText.length)}</pre>
             </div>
             <div className={"ty-next-char"}>
-                <pre><b><u>{nextChar === " " ? "_" : nextChar}</u></b></pre>
+                <pre><b><u>{nextChar}</u></b></pre>
             </div>
             <div className={"ty-target-text"}>
-                <pre>{targetText}</pre>
+                <pre>{nextToType.substr(0, 60)}</pre>
             </div>
             <div className={"ty-typed-text"}>
-                <pre>{typedText}</pre>
+                <pre>{typedText.substring(typedText.length - 60 > 0 ? typedText.length - 60 : 0, typedText.length)}</pre>
             </div>
             <input className={`ty-typing-input ${inputText.length > 0 && "ty-typing-input--typo"}`}
                    type={"text"}
