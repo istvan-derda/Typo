afa9c6b5c93b03cb6dfeeb52899b1f32f5805568
Istvan Derda <istvan.derda@stud.htwk-leipzig.de>
2020-08-17 17:28:50 +0200

    everything centered

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.scss';
 import TypingInterface from "./TypingInterface/TypingInterface";
 
 function App() {
-  return (
-    <div className="ty-app">
-      The beginning of Typo
-        <TypingInterface />
-    </div>
-  );
+    return (
+        <div className="ty-app">
+            <h1 className={"ty-app-title"}>The beginning of Typo</h1>
+            <div className={"ty-center"}>
+                <TypingInterface/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
